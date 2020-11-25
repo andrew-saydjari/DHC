32c3e1dd509b5b11ec0dbd6ecc451e4072d1d038
Andrew Saydjari <saydjari@g.harvard.edu>
2020-11-25 02:12:12 -0500

    testing night 1

---
+++ test.jl
@@ -0,0 +1,18 @@
+println("hello doug")
+
+print("end")
+
+Pkg.add("Plots")
+
+Pkg.add("PyPlot")
+
+Pkg.add("PlotThemes")
+
+Pkg.add("PlotlyJS")
+
+using Plots
+pyplot()
+
+plotlyjs()
+
+plot([1,1,1])
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Simple tests
+print('Hello world!')
+17+25
