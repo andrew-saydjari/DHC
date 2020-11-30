d4d3da68b7b9f259d52df4b87ea22e4b204a26cc
andrew-saydjari <saydjari@g.harvard.edu>
2020-11-29 21:04:07 -0500

    for github issue

---
+++ inline_test.jl
@@ -0,0 +1,7 @@
+## Both of these print to the commandline and do not display inline.
+display(1)
+print("hello world")
+
+## This does not plot inline. It only plots in the plotter gui.
+using Plots
+plot([1,2,1])
