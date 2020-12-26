80216c70043f6f3794f990f211476284b440378a
dfink <dfinkbeiner@cfa.harvard.edu>
2020-12-26 17:19:29 -0500

    DHC_BLAS things I should have checked in last week.

--- DHC_BLAS.jl
+++ DHC_BLAS.jl
@@ -10,18 +10,20 @@ using HybridArrays
 
 # put the cwd on the path so Julia can find the module
 push!(LOAD_PATH, pwd())
-using DHC_Utils
+using DHC_2DUtils
 
 ##
 
 # create filter bank
 @time fink_filter_set  = fink_filter_bank(8,8)
+@time fink_filter_set2  = fink_filter_bank(8,8,wid=2)
 
 # condense this to a filter list of (index, value) pairs.
 @time filter_list = fink_filter_list(fink_filter_set)
+@time filter_list2 = fink_filter_list(fink_filter_set2)
 
 # Generate phi from an L=16 filter bank
-donut = fink_filter_bank(8,8)
+donut = fink_filter_bank(8,16)
 phi   = sqrt.(reshape(sum(donut[:,:,8,:].^2,dims=[3,4]),256,256))
 phi_r = real(ifft(phi))
 # check that the real-space function sums to 1 as expected.
@@ -428,5 +430,242 @@ temp2 = speedy_DHC(test_img, filter_list)
 
 Profile.clear()
 @profile speedy_DHC(test_img, filter_list)
-
 Juno.profiler()
+
+
+
+
+
+
+
+function doug_DHC(image::Array{Float64,2}, filter_list)
+    # Use 2 threads for FFT
+    FFTW.set_num_threads(2)
+
+    # array sizes
+    (Nx, Ny)  = size(image)
+    (J,L)     = size(filter_list[1])
+
+    # allocate coeff arrays
+    out_coeff = []
+    S0  = zeros(Float64, 2)
+    S1  = zeros(Float64, J, L)
+    S20 = zeros(Float64, J, L, J, L)
+    S12 = zeros(Float64, J, L, J, L)
+    S2  = zeros(Float64, J, L, J, L)
+
+    # allocate image arrays for internal use
+    im_fdf_0_1 = zeros(Float64,           Nx, Ny, J, L)   # this must be zeroed!
+    im_rd_0_1  = Array{Float64, 4}(undef, Nx, Ny, J, L)
+
+    ## 0th Order
+    S0[1]   = mean(image)
+    norm_im = image.-S0[1]
+    S0[2]   = sum(norm_im .* norm_im)/(Nx*Ny)
+    norm_im ./= sqrt(Nx*Ny*S0[2])
+
+    append!(out_coeff,S0[:])
+
+    ## 1st Order
+    im_fd_0 = fft(norm_im)
+
+    # unpack filter_list
+    f_ind   = filter_list[1]  # (J, L) array of filters represented as index value pairs
+    f_val   = filter_list[2]
+
+    zarr = zeros(ComplexF64, Nx, Ny)  # temporary array to fill with zvals
+
+    # make a FFTW "plan" for an array of the given size and type
+    P   = plan_ifft(im_fd_0)   # P is an operator, P*im is ifft(im)
+
+    ## Main 1st Order and Precompute 2nd Order
+    for l = 1:L
+        for j = 1:J
+            S1tot = 0.0
+            f_i = f_ind[j,l]  # CartesianIndex list for filter
+            f_v = f_val[j,l]  # Values for f_i
+            # for (ind, val) in zip(f_i, f_v)   # this is slower!
+            if length(f_i) > 0
+                for i = 1:length(f_i)
+                    ind       = f_i[i]
+                    zval      = f_v[i] * im_fd_0[ind]
+                    S1tot    += abs2(zval)
+                    zarr[ind] = zval
+                    im_fdf_0_1[ind,j,l] = abs(zval)
+                end
+                S1[j,l] = S1tot
+                im_rd_0_1[:,:,j,l] .= abs2.(P*zarr)  # Inv FFT
+                zarr[f_i] .= 0
+            end
+        end
+    end
+    append!(out_coeff, S1[:])
+
+    # we stored the abs()^2, so take sqrt (this is faster to do all at once)
+    im_rd_0_1 .= sqrt.(im_rd_0_1)
+
+    ## 2nd Order
+    Amat = reshape(im_fdf_0_1, Nx*Ny, J*L)
+    S12  = reshape(Amat' * Amat, J, L, J, L)
+    Amat = reshape(im_rd_0_1, Nx*Ny, J*L)
+    S20  = reshape(Amat' * Amat, J, L, J, L)
+    append!(out_coeff, S20)
+    append!(out_coeff, S12)
+
+    ## Traditional second order
+
+    for l = 1:L
+        for j = 1:J
+            thisim = fft(im_rd_0_1[:,:,j,l])  # Could try rfft here
+            println("j ",j,"  l",l,"  sum(fft):",sum(abs2.(thisim))/65536, "  sum(im): ",sum(abs2.(im_rd_0_1[:,:,j,l])))
+            # Loop over (l2,j2) and do second-order convolution
+            for l2 = 1:L
+                for j2 = 1:J
+                    S2tot = 0.0
+                    f_i = f_ind[j2,l2]  # CartesianIndex list for filter
+                    f_v = f_val[j2,l2]  # Values for f_i
+
+                    S2tot = sum(abs2.(f_v .* thisim[f_i])) # sum im^2 same in real space
+                    S2[j,l,j2,l2] = S2tot
+                end
+            end
+        end
+    end
+
+
+    append!(out_coeff, S2)
+
+
+    return out_coeff
+end
+
+Profile.clear()
+@profile doug_DHC(test_img, filter_list)
+Juno.profiler()
+
+
+out = doug_DHC(test_rod, filter_list2)
+S2 = reshape(out[8259:12354],8,8,8,8)
+
+
+
+
+function rod(xcen, ycen, length, pa, fwhm)
+    rodimage = zeros(256,256)
+    nx = 256
+    x=0
+    y=0
+    sig = fwhm/2.355
+    dtor = π/180
+    # -------- define a unit vector in direction of rod at position angle pa
+    ux = sin(pa*dtor)   #  0 deg is up
+    uy = cos(pa*dtor)   # 90 deg to the right
+
+    for i=1:nx
+        for j=1:nx
+            x=i-nx/2+xcen
+            y=j-nx/2+ycen
+
+            # -------- distance parallel and perpendicular to
+            dpara =  ux*x + uy*y
+            dperp = -uy*x + ux*y
+
+            if abs(dpara)-length <0
+                dpara= 0
+            end
+            dpara = abs(dpara)
+            dpara = min(abs(dpara-length),dpara)
+
+            rodimage[i,j] = exp(-(dperp^2+dpara^2)/(2*sig^2))
+        end
+    end
+    return rodimage
+end
+
+@time test_rod = rod(10,10,30,45,4)
+
+
+function rod_test(filter_list)
+
+    angmax  = 180
+    angstep = 2
+    nang    = angmax ÷ angstep+1
+    energy  = zeros(Float64, nang)
+    Siso    = zeros(8,8,8,nang)
+    Sall    = zeros(8,8,8,8,nang)
+    Sisoodd    = zeros(8,8,8,nang)
+    Sisoeven   = zeros(8,8,8,nang)
+    for i = 1:nang
+        ang = i*angstep
+        rod_image = rod(10,10,30,ang,4)
+        # t = speedy_DHC(rod_image, filter_list)
+        t = doug_DHC(rod_image, filter_list)
+        S20 = reshape(t[66+1:66+4096],64,64)
+        S2  = reshape(t[8259:12354],8,8,8,8)
+        energy[i] = sum(diag(S20))
+        # Verify sum of diag(S20) = sum(S1)   (= energy)
+        println(i,"   ",energy[i],"   ",sum(t[3:66])/65536)
+
+
+        # what about j1, j2, Delta L
+        S = reshape(S20,8,8,8,8)  #  (j1, l1, j2, l2), symmetric in j and l
+        println(sum(S))
+        for j1 = 1:8
+            for j2 = 1:8
+                for l1 = 1:8
+                    for l2 = 1:8
+                        DeltaL = mod(l1-l2,8)
+                        Siso[j1,j2,DeltaL+1,i] += S[j1,l1,j2,l2]
+                        # Sall[j1,j2,l1,l2,i] = S[j1,l1,j2,l2]
+                        Sall[j1,j2,l1,l2,i] = S2[j1,l1,j2,l2]
+                        #println(Siso[j1,j2,DeltaL+1,i])
+                    end
+                end
+                for l1 = 1:2:7
+                    for l2 = 1:2:7
+                        DeltaL = mod(l1-l2,8)
+                        Sisoodd[j1,j2,DeltaL+1,i] += S[j1,l1,j2,l2]
+                    end
+                end
+                for l1 = 2:2:8
+                    for l2 = 2:2:8
+                        DeltaL = mod(l1-l2,8)
+                        Sisoeven[j1,j2,DeltaL+1,i] += S[j1,l1,j2,l2]
+                    end
+                end
+
+            end
+        end
+
+    end
+    println(std(Siso))
+    return (Siso, Sisoodd, Sisoeven, Sall)
+end
+
+function plot8(Siso, j1, j2)
+
+    nang = size(Siso)[end]
+    angstep = 180/(nang-1)
+    ang = collect(0:angstep:180)
+    val = Siso[j1,j2,1,:]
+    p = plot(ang, val/mean(val))
+    for Δl = 2:8
+        val = Siso[j1,j2,Δl,:]
+        plot!(p, ang, val/mean(val))
+    end
+    display(p)
+
+
+    return
+end
+
+
+Siso = rod_test(filter_list)
+Siso2 = rod_test(filter_list2)
+(Siso2, Sisoodd, Sisoeven, Sall) = rod_test(filter_list2)
+
+
+heatmap(Siso[:,:,1,14])
+plot(Siso[4,4,1,:]/Siso[4,4,1,1])
+
+plot8(Siso,3,4)
