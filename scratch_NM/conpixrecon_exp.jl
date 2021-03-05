ea4debf585ab1816c8a222ea4f0d76360e071f59
nmudur <nmudur@gmail.com>
2021-03-05 09:44:19 -0500

    S12 working with all floating pix

--- scratch_NM/conpixrecon_exp.jl
+++ scratch_NM/conpixrecon_exp.jl
@@ -10,6 +10,7 @@ using Printf
 using SparseArrays
 using Distributions
 using Measures
+using ImageFiltering
 #using Plotly
 #using Weave
 
@@ -704,13 +705,17 @@ coeff_type="S2")
 epsilon=1e-5
 dust = Float64.(readdust())
 dust = dust[1:256,1:256]
-img = dust[1:16, 1:16]
-Nx=16
+Nx=32
+img = dust[1:Nx, 1:Nx]
+
+
 Omega_bool = true
-test = img[1:Nx, 1:Nx]
 fhash = fink_filter_hash(1, 8, nx=Nx, pc=1, wd=1, Omega=Omega_bool)
 Nf = length(fhash["filt_index"])
-
+#fixmask = rand(Nx,Nx) .< 0.1
+noise = reshape(rand(Normal(0, std(img)), Nx^2), (Nx, Nx))
+init = img+ noise
+init = imfilter(init, Kernel.gaussian(1))
 #Mask cons
 s12w, covs12 = S12_weights(img, fhash, 100, iso=doiso)
 incl= (s12w.!=0)
@@ -718,9 +723,24 @@ invcovar = Diagonal(s12w.^(-2))
 s12targ = DHC_2DUtils.DHC_compute(img, fhash, doS2=false, doS20=false, doS12=true, norm=false, iso=doiso)
 #myfoogens20_100itn = img_reconfunc_old(init, s20targ,  invcovar, fixmask, "S20", Dict([("iterations", 100)]))
 incl[1:Nf+2] .= 0 #removing S0 and redundant S1
-S12recon = img_reconfunc_coeffmask(init, fhash, s12targ[incl], invcovar[incl, incl], fixmask, Dict([("iterations", 10)]), coeff_mask = incl,
+S12recon = img_reconfunc_coeffmask(init, fhash, s12targ[incl], invcovar[incl, incl], fixmask, Dict([("iterations", 100)]), coeff_mask = incl,
  coeff_type="S12")
-
+heatmap(img)
+heatmap(init)
+heatmap(S12recon)
+#plot_synth_QA(img, init, S12recon, fhash, fname="scratch_NM/TestPlots/s12_100itns.png")
+pl12 = plot(
+    heatmap(img, title="Ground Truth"),
+    heatmap(init, title="GT+N(0, std(I))"),
+    heatmap(S12recon,title= "Reconstruction w S12"),
+    heatmap(S12recon - img, title="Residual");
+    layout=4,
+)
+plot!(size=(1000, 800))
+savefig(pl12, "scratch_NM/TestPlots/s12_100itns_smoothedwhitenoiseinit.png")
+#Without smoothing
+mean(abs.(S12recon - img)) #2.54
+mean(abs.((S12recon - img)./img)) #0.03
 #=
 invcovarS2 = Diagonal(s2w.^(-2))
 invcovarS2 = invcovarS2[booltot_flat, booltot_flat]
