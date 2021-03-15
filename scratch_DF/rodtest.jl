d328125d36e3d296781739dbaba6965190251d64
dfink <dfinkbeiner@cfa.harvard.edu>
2021-03-14 21:13:14 -0400

    make rodtest plots to examine rotation invariance of S2iso and S20iso coeffs

---
+++ scratch_DF/rodtest.jl
@@ -0,0 +1,261 @@
+## Preloads
+using Statistics
+using FFTW
+using Plots
+using BenchmarkTools
+using Profile
+using LinearAlgebra
+using Measures
+#using StaticArrays
+#using HybridArrays
+
+# put the cwd on the path so Julia can find the module
+push!(LOAD_PATH, pwd()*"/main")
+using DHC_2DUtils
+
+##
+function rod_image(xcen, ycen, length, pa, fwhm; nx=256)
+    # returns image of a rod with some (x,y) position, length,
+    #   position angle, and FWHM in an (nx,nx) image.
+    rodimage = zeros(nx,nx)
+
+    x=0
+    y=0
+    sig = fwhm/2.355
+    dtor = π/180
+    # -------- define a unit vector in direction of rod at position angle pa
+    ux = sin(pa*dtor)   #  0 deg is up
+    uy = cos(pa*dtor)   # 90 deg to the right
+
+    for i=1:nx
+        for j=1:nx
+            x=i-nx/2+xcen
+            y=j-nx/2+ycen
+
+            # -------- distance parallel and perpendicular to
+            dpara =  ux*x + uy*y
+            dperp = -uy*x + ux*y
+
+            if abs(dpara)-length <0
+                dpara= 0
+            end
+            dpara = abs(dpara)
+            dpara = min(abs(dpara-length),dpara)
+
+            rodimage[i,j] = exp(-(dperp^2+dpara^2)/(2*sig^2))
+        end
+    end
+    rodimage ./= sqrt(sum(rodimage.^2))
+    return rodimage
+end
+
+
+function rod_test_old(filter_hash, Nsam=1)
+
+    Nf      = length(filter_hash["filt_value"])
+    Nj      = length(filter_hash["j_value"])
+    Nl      = length(filter_hash["theta_value"])
+    Npix    = filter_hash["npix"]
+    jlind   = filter_hash["psi_index"]
+    angmax  = 90.0
+    angstep = 2.5
+    nang    = Int64(angmax ÷ angstep+1)
+    energy     = zeros(nang)
+    Siso       = zeros(Nj, Nj, Nl, nang)
+    Sall       = zeros(Nl, Nj, Nl, Nj, nang)
+    S2all      = zeros(Nf, Nf, nang)
+    Ef         = zeros(Nf, nang)  # energy in each filter
+    dang       = 180/Nl
+    for i = 1:nang
+        ang = i*angstep
+
+
+        t = zeros(2+Nf+Nf*Nf)
+        for k=1:Nsam
+            rod1 = rod_image(10,10,30,ang+dang*k/4,8)
+            t   += DHC_compute(rod1, filter_hash)
+        end
+        t   ./= Nsam
+
+        #S20_big = reshape(t[2+Nf+1:2+Nf+Nf*Nf], Nf, Nf)
+        #S20 = S20_big[1:Nj*Nl, 1:Nj*Nl]
+        off2 = 2+Nf
+        S2 = reshape(t[off2+1:off2+Nf*Nf], Nf, Nf)
+        S2all[:,:,i] = S2
+        # energy[i] = sum(diag(S20_big))
+        # Ef[:,i]   = diag(S20_big)
+        # if filter_hash["pc"]==1 Ef[1:Nj*Nl,i] .*= 2 end
+        # Verify sum of diag(S20) = sum(S1)   (= energy)
+        # println(i,"   ",energy[i],"   ",sum(t[3:2+Nf]))
+
+
+        # what about j1, j2, Delta L
+        #S = reshape(S20,Nj,Nl,Nj,Nl)  #  (j1, l1, j2, l2), symmetric in j and l
+        S = S2[jlind,jlind]  # amazing this works...
+        println("Sum of S2:  ",sum(S2))
+        for j1 = 1:Nj
+            for j2 = 1:Nj
+                for l1 = 1:Nl
+                    for l2 = 1:Nl
+                        DeltaL = mod(l1-l2, Nl)
+                        Siso[j1,j2,DeltaL+1,i] += S[j1,l1,j2,l2]
+                        # Sall[j1,j2,l1,l2,i] = S[j1,l1,j2,l2]
+                        Sall[l1,j1,l2,j2,i] = S[j1,l1,j2,l2] # flip indices
+                        #println(Siso[j1,j2,DeltaL+1,i])
+                    end
+                end
+            end
+        end
+
+    end
+    println(std(Siso))
+    return (Siso, Sall)
+end
+
+
+function rod_test(filter_hash, Nsam=1; nx=256, doS20=false)
+    # Loop over many rod position angles, return Siso for all
+    Nf      = length(filter_hash["filt_value"])
+    Nj      = length(filter_hash["j_value"])
+    Nl      = length(filter_hash["theta_value"])
+    Npix    = filter_hash["npix"]
+    isomat  = filter_hash["S2_iso_mat"]
+    Niso    = size(isomat, 1)
+
+    angmax  = 180.0
+    angstep = 2.5
+    Nang    = Int64(angmax ÷ angstep+1)
+
+    Siso       = zeros(Niso, Nang)
+    dang       = 180/Nl
+    for i = 1:Nang
+        rod1 = zeros(nx,nx)
+        ang = i*angstep
+
+        t = zeros(2+Nf+Nf*Nf)
+        for k=1:Nsam
+            rod1 = rod_image(1,1,30*(nx/256),ang+dang*k/4,8,nx=nx)
+            rod1 .-= mean(rod1)
+            t   += DHC_compute(rod1, filter_hash, doS2=!doS20, doS20=doS20)
+        end
+        t   ./= Nsam
+
+        off2 = 2+Nf
+        S2 = t[off2+1:off2+Nf*Nf]
+
+        println(i," of ",Nang,"  Sum of S2:  ",sum(S2), "  Sum of rod: ",sum(rod1))
+        Siso[:,i] = isomat * S2
+
+    end
+    println(std(Siso))
+    return Siso
+end
+
+
+function rodtest_rms(Siso; frac=false)
+    # reshape(Sisoin,Nj*Nj*Nl,37)
+    sig = std(Siso, dims=2)
+    mn  = mean(Siso, dims=2)
+    if frac return sig./(mn.+1e-6) end
+    return sig
+end
+
+
+function rodtest_plot_rms(Siso1,Siso4,Siso16;outname="test.png",
+    label="Nsam=".*["1","4","16"], doS20=false, title=title)
+
+    ylim=10.0 .^(-6,0)
+    marg   = 10mm
+    coefftype = doS20 ? "S20" : "S2"
+    xlabel = coefftype*"_ISO Index"
+    p1=plot(rodtest_rms(Siso1,frac=true),ylim=ylim,xlabel=xlabel,
+            ylabel="Fractional RMS", yscale=:log10, line=(:gray, :dot),
+            label=label[1],size=(700,1000),leftmargin=marg)
+    plot!(p1,rodtest_rms(Siso4,frac=true), line=(:gray, :dash),label=label[2])
+    plot!(p1,rodtest_rms(Siso16,frac=true), line=(:blue),label=label[3])
+    annotate!(p1,10,0.6*ylim[2],text(title,:left,:blue,16))
+
+    p2=plot(rodtest_rms(Siso1),ylim=[1E-10,1E-2],xlabel=xlabel,
+            ylabel="RMS [Energy]", yscale=:log10, line=(:gray, :dot),
+            label=label[1],size=(700,1000))
+    plot!(p2,rodtest_rms(Siso4), line=(:gray, :dash),label=label[2])
+    plot!(p2,rodtest_rms(Siso16), line=(:green),label=label[3])
+
+    ps = [p1,p2]
+    myplot = plot(ps..., layout=(2,1))
+    savefig(myplot, outname)
+
+    return
+end
+
+
+
+function rodtest_plot_coeffs(Siso1,Siso4,Siso16;outname="test.png",
+    label="Nsam=".*["1","4","16"], doS20=false, title=title)
+
+    marg   = 4mm
+    coefftype = doS20 ? "S20" : "S2"
+    xval   = collect(0:2.5:180)
+    xlabel = "Angle [deg]"
+    ticks  = collect(0:45:180)
+
+    p1=plot(xval,Siso1',yscale=:log10,legend=false,xlabel=xlabel,
+             ylabel=coefftype,size=(700,1000),leftmargin=marg,xticks=ticks)
+    annotate!(p1,0,.9,text(title,:left,:blue,16))
+
+    p2=plot(xval,Siso16',yscale=:log10,legend=false,xlabel=xlabel,
+             ylabel=coefftype,size=(700,1000),leftmargin=marg,xticks=ticks)
+    #annotate!(p2,0,.9,text(title,:left,:blue,16))
+
+    p3=plot(xval,Siso1',yscale=:log10,legend=false,xlabel=xlabel,
+             ylabel=coefftype,size=(700,1000),leftmargin=marg,
+             xticks=ticks,ylims=(1e-3,1e-2))
+    annotate!(p3,0,.9,text(title,:left,:blue,16))
+
+    p4=plot(xval,Siso16',yscale=:log10,legend=false,xlabel=xlabel,
+             ylabel=coefftype,size=(700,1000),leftmargin=marg,
+             xticks=ticks,ylims=(1e-3,1e-2))
+    annotate!(p4,0,.9,text(title,:left,:blue,16))
+
+    ps = [p1,p2,p3,p4]
+    myplot = plot(ps..., layout=(2,2))
+    savefig(myplot, outname)
+
+    return
+end
+
+
+function rod_test_plot(Nsam; L=8, wd=1, nx=128,
+                       outname="test.png", doS20=false)
+    # run rod_test for 3 values of Nsam and output PNG file
+    fhash = fink_filter_hash(1, L, wd=wd, nx=nx, Omega=true)
+    Siso_A = rod_test(fhash, Nsam[1], nx=nx, doS20=doS20)
+    Siso_B = rod_test(fhash, Nsam[2], nx=nx, doS20=doS20)
+    Siso_C = rod_test(fhash, Nsam[3], nx=nx, doS20=doS20)
+    mylabels = "Nsam=".*string.(Nsam)
+    coefftype = doS20 ? "S20" : "S2"
+    title = string(coefftype, "iso, L=", L, ", wd=", wd)
+    rodtest_plot_rms(Siso_A, Siso_B, Siso_C, outname=outname,
+                     label=mylabels, title=title, doS20=doS20)
+
+    cname = replace(outname, "RMS" => "coeff")
+    rodtest_plot_coeffs(Siso_A, Siso_B, Siso_C, outname=cname,
+                        label=mylabels, title=title, doS20=doS20)
+
+    return
+end
+
+# Just run this code to make the plots I sent on Mar 14, 2021 (π-day!)
+nx=128
+snx = string(nx)
+Nsam = [1,4,16]
+
+rod_test_plot(Nsam; L=8, wd=1, nx=nx, outname="S2-RMS-NL8-wd1-"*snx*".png")
+rod_test_plot(Nsam; L=8, wd=2, nx=nx, outname="S2-RMS-NL8-wd2-"*snx*".png")
+rod_test_plot(Nsam; L=8, wd=4, nx=nx, outname="S2-RMS-NL8-wd4-"*snx*".png")
+rod_test_plot([1,4,32]; L=16, wd=4, nx=nx, outname="S2-RMS-NL16-wd4-"*snx*".png")
+
+rod_test_plot(Nsam; L=8, wd=1, nx=nx, outname="S20-RMS-NL8-wd1-"*snx*".png", doS20=true)
+rod_test_plot(Nsam; L=8, wd=2, nx=nx, outname="S20-RMS-NL8-wd2-"*snx*".png", doS20=true)
+rod_test_plot(Nsam; L=8, wd=4, nx=nx, outname="S20-RMS-NL8-wd4-"*snx*".png", doS20=true)
+rod_test_plot([1,4,32]; L=16, wd=4, nx=nx, outname="S20-RMS-NL16-wd4-"*snx*".png", doS20=true)
