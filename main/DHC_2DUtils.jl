5b461bb6060378007ad6ece55e6f7eaf07b52567
andrew-saydjari <saydjari@g.harvard.edu>
2021-03-16 20:13:40 -0400

    RGB_multichannel support

--- main/DHC_2DUtils.jl
+++ main/DHC_2DUtils.jl
@@ -868,6 +868,125 @@ module DHC_2DUtils
         return out_coeff
     end
 
+    function DHC_compute_RGB(image::Array{Float64}, filter_hash::Dict, filter_hash2::Dict=filter_hash;
+        doS20::Bool=true, norm=true, iso=false, FFTthreads=2)
+        # image        - input for WST
+        # filter_hash  - filter hash from fink_filter_hash
+        # filter_hash2 - filters for second order.  Default to same as first order.
+        # doS2         - compute S2 coeffs
+        # doS12        - compute S2 coeffs
+        # doS20        - compute S2 coeffs
+        # norm         - scale to mean zero, unit variance
+        # iso          - sum over angles to obtain isotropic coeffs
+
+        # Use 2 threads for FFT
+        FFTW.set_num_threads(FFTthreads)
+
+        # array sizes
+        (Nx, Ny, Nc)  = size(image)
+        if Nx != Ny error("Input image must be square") end
+        (Nf, )    = size(filter_hash["filt_index"])
+        if Nf == 0  error("filter hash corrupted") end
+        @assert Nx==filter_hash["npix"] "Filter size should match npix"
+        @assert Nx==filter_hash2["npix"] "Filter2 size should match npix"
+
+        # allocate coeff arrays
+        out_coeff = []
+        S0  = zeros(Float64, Nc*2)
+        S1  = zeros(Float64, Nc*Nf)
+
+        if doS20 S20 = zeros(Float64, Nf, Nf) end  # real space correlatio
+        # anyM2 = doS2 | doS12 | doS20
+        anyrd = doS20 #| doS2             # compute real domain with iFFT
+
+        # allocate image arrays for internal use
+        mean_im = zeros(Float64,1,1,Nc)
+        pwr_im = zeros(Float64,1,1,Nc)
+        norm_im = zeros(Float64,Nx,Ny,Nc)
+        im_fd_0 = zeros(ComplexF64, Nx, Ny, Nc)
+        im_fd_0_sl = zeros(ComplexF64, Nx, Ny)
+
+        if doS20
+            Amat1 = zeros(Nx*Ny, Nf)
+            Amat2 = zeros(Nx*Ny, Nf)
+        end
+
+        if anyrd im_rd_0_1  = Array{Float64, 4}(undef, Nx, Ny, Nf, Nc) end
+
+        ## 0th Order
+        mean_im = mean(image, dims=(1,2))
+        S0[1:Nc]   = dropdims(mean_im,dims=(1,2))
+        norm_im = image.-mean_im
+        pwr_im = sum(norm_im .* norm_im,dims=(1,2))
+        S0[1+Nc:end]   = dropdims(pwr_im,dims=(1,2))./(Nx*Ny)
+        if norm
+            norm_im ./= sqrt.(pwr_im)
+        else
+            norm_im = copy(image)
+        end
+
+        append!(out_coeff,S0[:])
+
+        ## 1st Order
+        im_fd_0 .= fft(norm_im,(1,2))  # total power=1.0
+
+        # unpack filter_hash
+        f_ind   = filter_hash["filt_index"]  # (J, L) array of filters represented as index value pairs
+        f_val   = filter_hash["filt_value"]
+
+        zarr = zeros(ComplexF64, Nx, Ny)  # temporary array to fill with zvals
+
+        # make a FFTW "plan" for an array of the given size and type
+        if anyrd
+            P = plan_ifft(zarr) end  # P is an operator, P*im is ifft(im)
+
+        ## Main 1st Order and Precompute 2nd Order
+        for f = 1:Nf
+            S1tot = 0.0
+            f_i = f_ind[f]  # CartesianIndex list for filter
+            f_v = f_val[f]  # Values for f_i
+            # for (ind, val) in zip(f_i, f_v)   # this is slower!
+            for chan = 1:Nc
+                im_fd_0_sl .= im_fd_0[:,:,chan]
+                if length(f_i) > 0
+                    for i = 1:length(f_i)
+                        ind       = f_i[i]
+                        zval      = f_v[i] * im_fd_0_sl[ind]
+                        S1tot    += abs2(zval)
+                        zarr[ind] = zval        # filter*image in Fourier domain
+                    end
+                    S1[f+(chan-1)*Nf] = S1tot/(Nx*Ny)  # image power
+                    if anyrd
+                        im_rd_0_1[:,:,f,chan] .= abs2.(P*zarr) end
+                    zarr[f_i] .= 0
+                end
+            end
+        end
+
+        if iso
+            S1M = filter_hash["S1_iso_mat"]
+            M1 = blockdiag(S1M,S1M,S1M)
+        end
+        append!(out_coeff, iso ? M1*S1 : S1)
+
+        # we stored the abs()^2, so take sqrt (this is faster to do all at once)
+        if anyrd im_rd_0_1 .= sqrt.(im_rd_0_1) end
+
+        # Real domain 2nd order
+        if doS20
+            for chan1 = 1:Nc
+                for chan2 = 1:Nc
+                    Amat1 = reshape(im_rd_0_1[:,:,:,chan1], Nx*Ny, Nf)
+                    Amat2 = reshape(im_rd_0_1[:,:,:,chan2], Nx*Ny, Nf)
+                    S20  = Amat1' * Amat2
+                    append!(out_coeff, iso ? filter_hash["S2_iso_mat"]*S20[:] : S20[:])
+                end
+            end
+        end
+
+        return out_coeff
+    end
+
     function DHC_compute_gpu_fake(image::Array{Float64,2}, filter_hash::Dict, filter_hash2::Dict=filter_hash;
         doS2::Bool=true, doS12::Bool=false, doS20::Bool=false, norm=true, iso=false,batch_mode::Bool=false,opt_memory::Bool=false,max_memory::Int64=0)
 
