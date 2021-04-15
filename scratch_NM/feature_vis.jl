793daf57b4941904e93a73e676b015a0cb889f03
nmudur <nmudur@gmail.com>
2021-04-15 14:58:42 -0400

    Updated feature vis

--- scratch_NM/feature_vis.jl
+++ scratch_NM/feature_vis.jl
@@ -26,113 +26,60 @@ using Data_Utils
 using Visualization
 using ReconFuncs
 
+#Rewritten
+logbool= false                                                #Calculate coefficients of the log of the image?
+apdbool = true                                                #Apodize?
+isobool = false                                               #Iso?
 
-#S1
-fname_save = "scratch_NM/NewWrapper/FeatureVis/NotIso/New"
+#Image you want to start with
 Nx=64
-im = readsfd(Nx)
-garbage_true = im[:, :, 34]
-init = fill(mean(im), (Nx, Nx))
+sfdall = readsfd_fromsrc("scratch_NM/data/dust10000.fits", Nx, logbool=false)
+input = sfdall[:, :, 5]
 
 filter_hash = fink_filter_hash(1, 8, nx=Nx, pc=1, wd=1, Omega=true)
-(S1iso, Nf) = size(filter_hash["S1_iso_mat"])
-(S2iso, Nfsq) = size(filter_hash["S2_iso_mat"])
-dhc_args = Dict(:doS2=>false, :doS12=>false, :doS20=>true, :apodize=>false, :iso=>false) #Iso #CHANGE: Change sig for the sfd data since the noise model is super high and the tiny values make sense
-if dhc_args[:iso]
-    coeff_mask = falses(2+S1iso+S2iso)
-    coeff_mask[S1iso+3:end] .= true
-else #Not iso
-    coeff_mask = falses(2+Nf+Nf^2)
-    coeff_mask[Nf+3:end] .= true
+dhc_args = Dict(:doS2=>false, :doS12=>false, :doS20=>true, :apodize=>apdbool, :iso=>isobool)
+lambda = 0.0
+optim_settings = Dict([("iterations", 1000), ("norm", false), ("minmethod", ConjugateGradient())])
+
+#Coefficient to optimize wrt
+Jvalue = 0
+Lvalue = 0
+#Code currently requires you to access S1 through the S2 matrix diagonal
+mask_nf = (filter_hash["J_L"][:, 1] .== Jvalue) .& (filter_hash["J_L"][:, 2] .== Lvalue)
+mask_nf2 = Diagonal(mask_nf)
+(Nf,) = size(filter_hash["filt_index"])
+coeff_mask = falses(2+Nf+Nf^2)
+coeff_mask[3+Nf:end] .= mask_nf2[:]
+
+#Setting a target coefficient: currently I'm taking the 10*true target coefficient
+#=
+
+if logbool
+    procdbn = log.(sfdall)
+else
+    procdbn = sfdall
 end
 
-#white_noise_args = Dict(:loc=>0.0, :sig=>std(true_img), :Nsam=>1000, :norm=>false, :smooth=>false, :smoothval=>0.8) #Iso #only if you're using noise based covar
-optim_settings = Dict([("iterations", 1000), ("norm", false), ("minmethod", ConjugateGradient())])
-recon_settings = Dict([("target_type", "sfd_dbn"), ("covar_type", "sfd_dbn"), ("log", false), ("GaussianLoss", true), ("Invcov_matrix", "Diagonal+Eps"),
-  ("optim_settings", optim_settings), ("lambda", 0.0)]) #Removed white_noise_args
-#regs_true = DHC_compute_wrapper(log.(true_img), filter_hash; norm=false, dhc_args...)[coeff_mask]
-s2mean, s2icov = meancov_generator(garbage_true, filter_hash, dhc_args, coeff_mask, recon_settings, safety=nothing)
-#Check
-#s2mean - s_true
+dbncoeffs = get_dbn_coeffs(procdbn, filter_hash, dhc_args, coeff_mask = coeff_mask)
+sigval = std(dbncoeffs, dims=1)
+=#
+if logbool
+    s_targ_mean = 10.0*DHC_compute_wrapper(log.(input), filter_hash, norm=false; dhc_args...)[coeff_mask]             #target coefficient vector = twice the current wst value
+    s_targ_invcov = Diagonal([0.1.^(-2)])
 
-recon_settings["s_targ_mean"] = s2mean
-recon_settings["s_invcov"] = s2icov
-recon_settings["safemode"] = false
-recon_settings["fname_save"] = fname_save * ".jld2"
-recon_settings["optim_settings"] = optim_settings
-resobj, recon_img = reconstruction_wrapper(garbage_true, init, filter_hash, dhc_args, coeff_mask, recon_settings)
+else
+    s_targ_mean = 10.0*DHC_compute_wrapper(input, filter_hash, norm=false; dhc_args...)[coeff_mask]             #target coefficient vector = twice the current wst value
+    s_targ_invcov = Diagonal([0.0015.^(-2)])          #Might need to tune. inverse covariance matrix with sigma= true_coeff/10 for J=1 L=2
 
-p= plot([t.value for t in Optim.trace(resobj)])
-plot!(title="Loss: S20 only | Targ = S(SFD) | Cov = SFD Covariance", xlabel = "No. Iterations", ylabel = "Loss")
-savefig(p, fname_save * "_trace.png")
-
-p=heatmap(recon_img, title="Init: Mean SFD, Optimizing only wrt SFD S20")
-savefig(p, fname_save * "_result.png")
-#Visualization.plot_diffscales([apodizer(true_img), apodizer(init), apodizer(recon_img), apodizer(recon_img) - apodizer(true_img)], ["GT", "Init", "Reconstruction", "Residual"], fname=fname_save*".png")
-#Visualization.plot_synth_QA(apodizer(true_img), apodizer(init), apodizer(recon_img), fname=fname_save*"_6p.png")
-
-maximum(s2mean)
-minimum(s2mean)
-maximum(s2icov)
-minimum(diag(s2icov))
-
-#S1_Target
-fname_save = "scratch_NM/NewWrapper/FeatureVis/NotIso/meaninit_allS1_target"
-Nx=64
-im = readsfd(Nx)
-true_img = im[:, :, 1]
-init = fill(mean(im), (Nx, Nx))
-#heatmap(init)
-#heatmap(true_img)
-
-filter_hash = fink_filter_hash(1, 8, nx=Nx, pc=1, wd=1, Omega=true)
-(S1iso, Nf) = size(filter_hash["S1_iso_mat"])
-(S2iso, Nfsq) = size(filter_hash["S2_iso_mat"])
-dhc_args = Dict(:doS2=>false, :doS12=>false, :doS20=>true, :apodize=>true, :iso=>false) #Iso #CHANGE: Change sig for the sfd data since the noise model is super high and the tiny values make sense
-if dhc_args[:iso]
-    coeff_mask = falses(2+S1iso+S2iso)
-    coeff_mask[S1iso+3:end] .= true
-else #Not iso
-    coeff_mask = falses(2+Nf+Nf^2)
-    coeffsS20 = falses(Nf, Nf)
-    coeffsS20[diagind(coeffsS20)] .= true
-    coeff_mask[Nf+3:end] .= coeffsS20[:]
 end
 
-#white_noise_args = Dict(:loc=>0.0, :sig=>std(true_img), :Nsam=>1000, :norm=>false, :smooth=>false, :smoothval=>0.8) #Iso #only if you're using noise based covar
-optim_settings = Dict([("iterations", 1000), ("norm", false), ("minmethod", ConjugateGradient())])
-recon_settings = Dict([("target_type", "ground_truth"), ("covar_type", "sfd_dbn"), ("log", false), ("GaussianLoss", true), ("Invcov_matrix", "Diagonal+Eps"),
-  ("optim_settings", optim_settings), ("lambda", 0.0)]) #Removed white_noise_args
-#regs_true = DHC_compute_wrapper(log.(true_img), filter_hash; norm=false, dhc_args...)[coeff_mask]
-s2mean, s2icov = meancov_generator(true_img, filter_hash, dhc_args, coeff_mask, recon_settings, safety=nothing)
-#Check
-#s2mean - s_true
 
-recon_settings["s_targ_mean"] = s2mean
-recon_settings["s_invcov"] = s2icov
-recon_settings["safemode"] = false
-recon_settings["fname_save"] = fname_save * ".jld2"
-recon_settings["optim_settings"] = optim_settings
-resobj, recon_img = reconstruction_wrapper(true_img, init, filter_hash, dhc_args, coeff_mask, recon_settings)
+if !logbool
+    res, recon_img = Deriv_Utils_New.image_recon_derivsum_regularized(input, filter_hash, s_targ_mean, s_targ_invcov, falses(Nx, Nx), dhc_args, optim_settings=optim_settings, coeff_mask=coeff_mask, lambda=lambda)
+else #Log=true
+    res, recon_img = Deriv_Utils_New.image_recon_derivsum_regularized(log.(input), filter_hash, s_targ_mean, s_targ_invcov, falses(Nx, Nx), dhc_args, coeff_mask=coeff_mask, optim_settings=optim_settings, lambda=lambda)
+    recon_img = exp.(recon_img)
+end
 
-p= plot([t.value for t in Optim.trace(resobj)])
-plot!(title="Loss: S1 only | Targ = S(SFD) | Cov = SFD Covariance", xlabel = "No. Iterations", ylabel = "Loss")
-savefig(p, fname_save * "_trace.png")
-
-p=heatmap(recon_img, title="Init: Mean SFD, Optimizing only wrt SFD S1")
-savefig(p, fname_save * "_result.png")
-Visualization.plot_diffscales([apodizer(true_img), apodizer(init), apodizer(recon_img), apodizer(recon_img) - apodizer(true_img)], ["GT", "Init", "Reconstruction", "Residual"], fname=fname_save*".png")
-
-#Visualization.plot_synth_QA(apodizer(true_img), apodizer(init), apodizer(recon_img), fname=fname_save*"_6p.png")
-
+heatmap(input)
 heatmap(recon_img)
-
-recon_img[1, 2]
-fname = "scratch_NM/NewWrapper/FeatureVis/NotIso/meaninit_allS1"
-gttarget = load(fname*".jld2")
-
-
-##4-2 KLDiv Examination\
-Nx=64
-regsfd = readsfd(Nx, logbool=false)
-dbn_coeffs_calc()
